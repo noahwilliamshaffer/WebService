164d3241692c40bac67a714cf12024801a44aa08
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2023-11-05 18:14:59 -0500

    Added Lambda function

--- HealthTrackerLambda/src/main/java/com/yourdomain/App.java
+++ HealthTrackerLambda/src/main/java/com/yourdomain/App.java
@@ -1,9 +1,15 @@
-package com.yourdomain;
+package com.noahwilliamshaffer;
 
-/**
- * Hello world!
- *
- */
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class UserRegistrationHandler implements RequestHandler<YourRequestClass, YourResponseClass> {
+    public YourResponseClass handleRequest(YourRequestClass request, Context context) {
+        // Your logic here
+        return new YourResponseClass();
+    }
+}
+
 public class App 
 {
     public static void main( String[] args )
