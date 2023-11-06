aca246475bf3948b3a282b24163fdcd81d67114c
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2023-11-06 15:24:12 -0500

    Added Hello World Function ready to merge to main.

--- src/main/java/com/noahshaffer/functions/HelloWorldFunction.java
+++ src/main/java/com/noahshaffer/functions/HelloWorldFunction.java
@@ -3,6 +3,13 @@ package com.noahshaffer.functions;
 //Functions: This folder likely contains your AWS Lambda function handlers.
 // Each function should correspond to a specific task or API endpoint.
 
-public class HelloWorldFunction {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class HelloWorldFunction implements RequestHandler<Object, String> {
     
+    @Override
+    public String handleRequest(Object input, Context context) {
+        return "Hello, World!";
+    }
 }
